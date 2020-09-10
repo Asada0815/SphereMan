a57595f163d2217ee56e295f0a4dd78d9602f1f0
Asada0815 <asada@posl.ait.kyushu-u.ac.jp>
2020-09-10 20:04:32 +0900

    add files

---
+++ Assets/Scripts/Level/FixedFieldParts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Level {
+
+    public enum FixedFieldPartsType {
+        blank,
+        wall,
+        goal,
+        ladder,
+        
+    }
+
+    public class FixedFieldParts : MonoBehaviour {
+        void Start() {
+            
+        }
+
+        void Update() {
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/Level/LevelField.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelField : MonoBehaviour {
+
+    void Start() {
+        
+    }
+
+
+
+    void Update() {
+        
+    }
+}
